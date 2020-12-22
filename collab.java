a491e0a1a2465e5998543c2f301952cf68202ca1
suhas <suhas.v@apisero.com>
2020-12-22 21:58:15 +0530

    NEW COMMIT

--- collab.java
+++ collab.java
@@ -1,5 +1,5 @@
 public class collab {
 	public static void main(String[] args) {
-		System.out.println("Hello everyone");
+		System.out.println("Hello Every1");
 } 		System.out.println("Hello this is Suhas");
 }
